06dd732652252baf439942cc5e6bcec46cbded85
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2023-09-22 22:58:42 +0300

    bug fix on db.json and context.js files

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,5 @@
-import { createContext, useState, axios } from "react";
-
+import { createContext, useState } from "react";
+import axios from "axios";
 const BooksContext = createContext();
 
 const Provider = ({ children }) => {
@@ -7,46 +7,64 @@ const Provider = ({ children }) => {
 
   //sayfa yüklendiğinde kayıtlı olan kitapları çağırır
   const fetchBooks = async () => {
-    const responce = await axios.get("http://localhost:3001/books");
-    setBooks(responce.data);
+    try {
+      const responce = await axios.get("http://localhost:3001/books");
+      setBooks(responce.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   //edit books
   const editBook = async (id, newTitle) => {
     //sayfa reflesh olduğunda yaptığımız editler kalıcı olur
-    const responce = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
+    try {
+      const responce = await axios.put(`http://localhost:3001/books/${id}`, {
+        title: newTitle,
+      });
 
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...responce.data };
-      }
-      return book;
-    });
-    setBooks(updatedBooks);
+      const updatedBooks = books.map((book) => {
+        if (book.id === id) {
+          return { ...book, ...responce.data };
+        }
+        return book;
+      });
+      setBooks(updatedBooks);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   //delete books
   const deleteBook = async (id) => {
     //sayfa reflesh olduğunda sildiğimiz booklar kalıcı olarak silinir
-    await axios.delete(`http://localhost:3001/books/${id}`);
+    try {
+      await axios.delete(`http://localhost:3001/books/${id}`);
 
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(updatedBooks);
+      const updatedBooks = books.filter((book) => {
+        return book.id !== id;
+      });
+      setBooks(updatedBooks);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   //create books
   const createBook = async (newTitle) => {
+    console.log(newTitle);
     //api üzerinden verimizi gönderiyoruz
-    const responce = await axios.post("http://localhost:3001/books", {
-      title: newTitle,
-    });
+    const tempVar = JSON.stringify(newTitle);
+    try {
+      const responce = await axios.post("http://localhost:3001/books", {
+        title: tempVar,
+      });
 
-    const updatedBooks = [...books, responce.data];
-    setBooks(updatedBooks);
+      const updatedBooks = [...books, responce.data];
+      setBooks(updatedBooks);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const sharedValues = {
